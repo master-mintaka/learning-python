6a788469d8f520e8c802852e1c8ae93d3cb188fb
John W Granados A - Master Mintaka <jwga76@gmail.com>
2023-11-19 20:18:24 -0500

    First and second simple scripts

---
+++ data-types.py
@@ -0,0 +1,11 @@
+#First script calculating the area of something
+length = 10
+width = 20
+area = length * width
+print("Te area is: " + str(area))
+
+#Simple tax calculator
+amount = 10
+tax = 0.19
+total = amount + amount * tax
+print(total)
