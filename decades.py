79e624f5b2418904263543d26c3f919a6d4957c9
John W Granados A - Master Mintaka <jwga76@gmail.com>
2023-11-21 22:19:39 -0500

    Decades calculator

---
+++ decades.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: \n"))
+
+decades = age // 10
+years = age % 10
+
+print("I have "+ str(decades) + " decades and " + str(years) + " years old")
+
+
---
+++ use-strings.py
@@ -0,0 +1,4 @@
+salutation = "Hello"
+name_1 = input("What's your name?\Johnn")
+greeting = salutation + " " + name_1
+print(greeting)
